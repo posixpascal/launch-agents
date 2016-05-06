dd2398f07ac7e20b6c6a1b59c4921e2e68642365
Pascal Raszyk <pr@mediatrooper.de>
2016-05-06 23:32:46 +0200

    Initial Commit

---
+++ LaunchControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LaunchControl
+//
+//  Created by Pascal on 06.05.16.
+//  Copyright © 2016 Pascal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LaunchControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LaunchControl
+//
+//  Created by Pascal on 06.05.16.
+//  Copyright © 2016 Pascal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
